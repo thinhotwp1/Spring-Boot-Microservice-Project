ec643d6cac9367ef72e7e440eacc901c1222808d
thinhotwp1 <61654110+thinhotwp1@users.noreply.github.com>
2023-01-06 08:46:36 +0700

    'update'

--- microservice-new/api-gateway/src/test/java/com/learn/apigateway/DiscoveryServiceApplicationTests.java
+++ microservice-new/api-gateway/src/test/java/com/learn/apigateway/DiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learn.discoveryservice;
+package com.learn.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- microservice-new/discovery-service/src/main/java/com/learn/apigateway/DiscoveryServiceApplication.java
+++ microservice-new/discovery-service/src/main/java/com/learn/apigateway/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.learn.discoveryservice;
+package com.learn.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
